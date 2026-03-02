95aa10ee4cb782fd410825bf1a6b8512399aee54
Arngrimur Bjarnason <arngrimur.bjarnason@zimpler.com>
2026-03-02 09:40:19 +0100

    [main] adding swagger docs

--- bookings/internal/pkg/web/router.go
+++ bookings/internal/pkg/web/router.go
@@ -9,13 +9,32 @@ import (
 	"syscall"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
 
 	"github.com/arngrimur/bilcool_monolith/bookings/internal/pkg/application"
 	"github.com/arngrimur/bilcool_monolith/bookings/internal/pkg/domain"
+	"github.com/gin-gonic/gin"
 )
 
+// @title           Swagger Example API
+// @version         1.0
+// @description     This is a sample server celler server.
+// @termsOfService  http://swagger.io/terms/
+
+// @contact.name   API Support
+// @contact.url    http://www.swagger.io/support
+// @contact.email  support@swagger.io
+
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @host      localhost:8080
+// @BasePath  /api/v1
+
+// @securityDefinitions.basic  BasicAuth
+
+// @externalDocs.description  OpenAPI
+// @externalDocs.url          https://swagger.io/resources/open-api/
 type httpRouter struct {
 	router  *gin.Engine
 	queries application.Queries
@@ -31,8 +50,8 @@ func NewRouter(q application.Queries) *httpRouter {
 		c.String(http.StatusOK, "pong")
 	})
 
-	h.router.GET("/api/bookings", h.getAllBookings)
-	h.router.GET("/api/bookings/:id", h.getBooking)
+	h.router.GET("/api/v1/bookings", h.getAllBookings)
+	h.router.GET("/api/v1/bookings/:id", h.getBooking)
 	return h
 }
 
